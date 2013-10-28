1a856de305706e8e69a4a097ffc5b0a534d52539
Xiao Yu <cutefish.yx@gmail.com>
2013-10-28 19:58:55 +0000

    modified:   scripts/fastpaxos.py

--- scripts/fastpaxos.py
+++ scripts/fastpaxos.py
@@ -19,27 +19,45 @@ def getGenerator(gcfg):
     return RandInterval.get(key, mean, cfg)
 
 def printSlot(j):
+    global prevtime
     curr = time.time()
-    if curr - prevTime > 5:
+    if curr - prevtime > 5:
         print 'on slot %s'%j
         prevtime = curr
 
-def computeY(j, X, Y, A, V, B, E, lGen):
+def computeYInd(j, X, Y, A, V, B, E, lGen):
     finish = True
     for i in range(len(Y[j])):
         if E[i] != -1:
+            #if already end
+            assert j > E[i]
+            Y[j][i] = infinite
+        elif X[i] <= V[j-1][i]:
+            assert B[i] != -1 and j > B[i]
+            assert Y[j-1][i] >= A[j-1], \
+                    ('Y[%s-1][%s] =%s >= %s = A[%s-1] '
+                     'j=%s, B[%s]=%s'
+                     %(j, i, Y[j-1][i], A[j-1], j, j, i, B[i]))
+            Y[j][i] = V[j-1][i] + lGen.next()
+        else:
+            Y[j][i] = X[i] + lGen.next()
+            finish = False
+    return finish
+
+def computeYNImm(j, X, Y, A, V, B, E, lGen):
+    finish = True
+    for i in range(len(Y[j])):
+        if E[i] != -1:
+            #already end
             assert j > E[i]
             Y[j][i] = infinite
         elif B[i] != -1:
+            #already begin but failed
             assert j > B[i]
-            assert X[i] <= V[j-1][i] and Y[j-1][i] >= A[j-1], \
-                    ('X[%s] = %s <= %s = V[%s-1][%s] and '
-                     'Y[%s-1][%s] =%s >= %s = A[%s-1] '
-                     'j=%s, B[%s]=%s'
-                     %(i, X[i], V[j-1][i], j, i, j, i, Y[j-1][i], A[j-1],j,
-                       j, i, B[i]))
             Y[j][i] = V[j-1][i] + lGen.next()
+            finish = False
         else:
+            #not begin but possbily begin on this slot
             Y[j][i] = X[i] + lGen.next()
             finish = False
     return finish
@@ -101,7 +119,7 @@ def runSingleAcceptorFailDiscarded(nprop, mean, lcfg, debug=False):
         V.append([0] * nprop)
         finish = True
         #compute Y
-        finish = computeY(j, X, Y, A, V, B, E, lGen)
+        finish = computeYInd(j, X, Y, A, V, B, E, lGen)
         if finish:
             break
         #compute A
@@ -141,7 +159,7 @@ def runSingleAcceptorFailDiscarded(nprop, mean, lcfg, debug=False):
     print 'fail.latency.std=%s'%numpy.std(failLatencies)
     print 'fail.latency.histo=(%s, %s)'%numpy.histogram(failLatencies)
 
-def runSingleAcceptorFailRestart(nprop, mean, lcfg, debug=False):
+def runSingleAcceptorFailRestartInd(nprop, mean, lcfg, debug=False):
     #generate X_i
     expoGen = RandInterval.get('expo', mean)
     X = [expoGen.next()]
@@ -163,7 +181,7 @@ def runSingleAcceptorFailRestart(nprop, mean, lcfg, debug=False):
         Y.append([0] * nprop)
         V.append([0] * nprop)
         #compute Y
-        computeY(j, X, Y, A, V, B, E, lGen)
+        computeYInd(j, X, Y, A, V, B, E, lGen)
         #compute A
         mval, midx = computeA(j, Y, A)
         #compute V
@@ -215,6 +233,79 @@ def runSingleAcceptorFailRestart(nprop, mean, lcfg, debug=False):
     print 'fail.latency.std=%s'%numpy.std(F)
     print 'fail.latency.histo=(%s, %s)'%numpy.histogram(F)
 
+def runSingleAcceptorFailRestartNImm(nprop, mean, lcfg, debug=False):
+    #generate X_i
+    expoGen = RandInterval.get('expo', mean)
+    X = [expoGen.next()]
+    for i in range(1, nprop):
+        X.append(expoGen.next() + X[i-1])
+    #run
+    lGen = getGenerator(lcfg)
+    A = [0] * nprop
+    Y = []
+    V = []
+    B = [-1] * nprop
+    E = [-1] * nprop
+    prevTime = time.time()
+    for j in range(nprop):
+        curr = time.time()
+        if curr - prevTime > 5:
+            print 'on slot %s'%j
+            prevTime = curr
+        Y.append([0] * nprop)
+        V.append([0] * nprop)
+        #compute Y
+        computeYNImm(j, X, Y, A, V, B, E, lGen)
+        #compute A
+        mval, midx = computeA(j, Y, A)
+        #compute V
+        for i in range(nprop):
+            V[j][i] = A[j] + lGen.next()
+        #compute B and E
+        for i in range(nprop):
+            if X[i] < A[j] and B[i] == -1:
+                B[i] = j
+        E[midx] = j
+        if debug:
+            printXYAVBE(j, X, Y, A, V, B, E)
+            print
+    #compute stats
+    R = [0] * nprop
+    T = [0] * nprop
+    S = []
+    F = []
+    for i in range(nprop):
+        R[i] = E[i] - B[i]
+        T[i] = V[E[i]][i] - X[i]
+        if B[i] != E[i]:
+            F.append(V[B[i]][i] - X[i])
+        for k in range(B[i] + 1, E[i]):
+            F.append(V[k][i] - V[k-1][i])
+        if B[i] == E[i]:
+            S.append(V[E[i]][i] - X[i])
+        else:
+            S.append(V[E[i]][i] - V[E[i] - 1][i])
+    if debug:
+        strs = ['Yk:']
+        for i in range(nprop):
+            strs.append('%.2f'%Y[E[i]][i])
+        print ' '.join(strs)
+        strs = ['T:']
+        for i in range(nprop):
+            strs.append('%.2f'%T[i])
+        print ' '.join(strs)
+    print 'nretries.mean=%s'%numpy.mean(R)
+    print 'nretries.std=%s'%numpy.std(R)
+    print 'nretries.histo=(%s, %s)'%numpy.histogram(R)
+    print 'total.latency.mean=%s'%numpy.mean(T)
+    print 'total.latency.std=%s'%numpy.std(T)
+    print 'total.latency.histo=(%s, %s)'%numpy.histogram(T)
+    print 'succ.latency.mean=%s'%numpy.mean(S)
+    print 'succ.latency.std=%s'%numpy.std(S)
+    print 'succ.latency.histo=(%s, %s)'%numpy.histogram(S)
+    print 'fail.latency.mean=%s'%numpy.mean(F)
+    print 'fail.latency.std=%s'%numpy.std(F)
+    print 'fail.latency.histo=(%s, %s)'%numpy.histogram(F)
 
 def main():
     if len(sys.argv) < 5:
@@ -222,7 +313,9 @@ def main():
         print 'fastpaxos <key> <num proposal> <mean> <lcfg> [--debug]'
         print '          key: '
         print '                 safd    -- runSingleAcceptorFailDiscarded'
-        print '                 safr    -- runSingleAcceptorFailRestart'
+        print '                 safri   -- runSingleAcceptorFailRestartInd'
+        print '                 safrm   -- runSingleAcceptorFailRestartMax'
+        print '                 safrn   -- runSingleAcceptorFailRestartNImm'
         print
         sys.exit(-1)
     debug = False
@@ -235,8 +328,12 @@ def main():
     lcfg = eval(sys.argv[4])
     if key == 'safd':
         runSingleAcceptorFailDiscarded(nprop, mean, lcfg, debug)
-    elif key == 'safr':
-        runSingleAcceptorFailRestart(nprop, mean, lcfg, debug)
+    elif key == 'safri':
+        runSingleAcceptorFailRestartInd(nprop, mean, lcfg, debug)
+    elif key == 'safrn':
+        runSingleAcceptorFailRestartNImm(nprop, mean, lcfg, debug)
+    else:
+        raise ValueError('unknown key:%s'%key)
 
 if __name__ == '__main__':
     main()
